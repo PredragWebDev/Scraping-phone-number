4fb75e27873915771232f41e0f5921ccf2df3b95
PredragWebDev <pedjolinodev@gmail.com>
2023-07-14 12:19:37 -0600

    modify

--- class/Scrap.js
+++ class/Scrap.js
@@ -5,13 +5,12 @@ const fs = require('fs');
 const cheerio = require("cheerio");
 const { promises } = require('dns');
 
-const url = 'https://www.remax.com/real-estate-agents';
+const url = 'https://www.remax.com/real-estate-agents?filters={page:1,count:96,sortBy:lastName}';
 
 const getPhone_number = async ( from, to) => {
   let count = 0;
   let result = [];
   let isError = false;
-  let chromeOptions = new chrome.Options();
   // chromeOptions.headless();
   
   do {
@@ -20,103 +19,35 @@ const getPhone_number = async ( from, to) => {
 
       let browser = await puppeteer.launch({ headless: false });
 
-      // let residential, bed, bath, price, currency, frequency, locate, area, studio;
+      let name, phone;
 
-  
       let page = await browser.newPage();
-    
-      await page.goto(url, { waitUntil: 'networkidle0', timeout: 0 });
+      
+      for (let i = from; i < to; i++) {
+        
+        await page.goto(`https://www.remax.com/real-estate-agents?filters={page:${i+1},count:96,sortBy:lastName}`, { waitUntil: 'networkidle0', timeout: 0 });
 
-      await page.select('.dselect-select', '96');
-      // await page.click('button[aria-label="Open Menu"]')
-      // await page.click('#signInLink');
-      // await page.waitForNavigation({ waitUntil: 'networkidle0' });
+        const mans = await page.$$('div[data-test="agent-card"]');
   
-      // await page.goForward();
-      // await page.type("#userName", "leobrown0921");
-      // await page.type("#password", "superage0611");
-      // await page.click('#login');
+        for (let card of mans ) {
+  
+          name = "";
+          phone = "";
+  
+          phone = await card.$eval('a[data-test="agent-card-phone"]', a => a.textContent);
+          name = await card.$eval('a[data-test="agent-card-name"]', a => a.textContent);
+  
+          const card_detail = {
+            name,
+            phone
+          }
+  
+          result.push(card_detail);
+        }
+      }
 
-      // await page.waitForNavigation({ waitUntil: 'networkidle0' });
-  
-      // await page.goForward();
 
-      // await page.goto("https://www.familysearch.org/search/", { waitUntil: 'networkidle0', timeout: 0 })
-
-      // const a_tags = await page.$$('a');
-      // for (tag of a_tags) {
-      //   let textContent = await page.evaluate(el => el.textContent, tag);
-      //   if (textContent === 'Find') {
-      //     tag.click();
-      //   }
-      // }
-    
-      // await page.waitForTimeout(3000);
-
-      // for (let i = from; i < to; i ++) {
-
-      //   if (i > 0) {
-
-      //     const _url = `https://www.bayut.com/to-rent/property/uae/page-${i+1}/`;
-  
-      //     await page.goto(_url, { waitUntil: 'networkidle0', timeout: 0 });
-  
-      //     await page.waitForTimeout(3000);
-      //   }
-
-      //   const list_of_properties = await page.$$("li[role = 'article']");
-  
-      //   for (let property of list_of_properties) {
-      //     residential = "";
-      //     bed = "";
-      //     studio = "";
-      //     bath= "";
-      //     area= "";
-      //     locate = "";
-      //     price = "";
-      //     currency = "";
-      //     frequency = "";
-  
-      //     residential = await property.$eval("span[aria-label = 'Type']", span => span.textContent);
-  
-      //     const exist_bed = await property.$("span[aria-label = 'Beds'")
-      //     if (exist_bed !== null) 
-      //     {
-      //       bed = await property.$eval("span[aria-label = 'Beds']", span => span.textContent);
-  
-      //     }
-      //     else {
-      //       studio = await property.$eval("span[aria-label = 'Studio']", span => span.textContent);
-      //     }
-  
-      //     bath = await property.$eval("span[aria-label = 'Baths']", span => span.textContent);
-      //     area = await property.$eval("span[aria-label = 'Area']", span => span.textContent);
-      //     const location_tag = await property.$("div[aria-label = Location");
-      //     locate = await location_tag.$eval("div._00a37089", div => div.textContent);
-  
-      //     price = await property.$eval("span[aria-label = 'Price']", span => span.textContent);
-      //     currency = await property.$eval("span[aria-label = 'Currency']", span => span.textContent);
-      //     frequency = await property.$eval("span[aria-label = 'Frequency']", span => span.textContent);
-  
-      //     const property_detail = {
-      //       residential,
-      //       bed,
-      //       studio,
-      //       bath,
-      //       area,
-      //       locate,
-      //       price,
-      //       currency,
-      //       frequency
-      //     }
-  
-      //     result.push(property_detail);
-  
-      //     console.log("tesxts>>>", property_detail);
-      //   }
-      // }
-
-      // browser.close();
+      browser.close();
 
       isError = false;
     
